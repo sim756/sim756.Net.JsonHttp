d72fcd88c633f6c8963d1ec14c36a1f42836ede3
Sadequl Islam Mithun <sim756@outlook.com>
2018-05-24 11:07:40 +0600

    base

---
+++ sim756.Net.JsonHttp/sim756.Net.JsonHttp/Exceptions/UrlEmptyException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sim756.Net.JsonHttp.Exceptions
+{
+    public class UrlEmptyException : Exception
+    {
+        public UrlEmptyException()
+        {
+        }
+
+        public UrlEmptyException(string message) : base(message)
+        {
+        }
+
+        public UrlEmptyException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
---
+++ sim756.Net.JsonHttp/sim756.Net.JsonHttp/JsonHttpClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+using sim756.Net.JsonHttp.Exceptions;
+
+namespace sim756.Net.JsonHttp
+{
+    public class JsonHttpClient<T>
+    {
+        public string Url { get; set; }
+
+        public JsonHttpClient()
+        {
+
+        }
+
+        public JsonHttpClient(string url)
+        {
+            this.Url = url;
+        }
+
+
+        public static void Post()
+        {
+
+        }
+
+        public T GetSerializedObject()
+        {
+            if (string.IsNullOrEmpty(Url))
+            {
+                throw new UrlEmptyException();
+            }
+
+            return JsonConvert.DeserializeObject<T>(new WebClient().DownloadString(Url));
+            //return default(T);
+        }
+    }
+}
