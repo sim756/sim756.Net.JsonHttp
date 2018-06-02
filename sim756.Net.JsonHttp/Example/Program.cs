0b2653bd92fd2e15d272b3e29edcf8db9da6817d
sim756 <sim756@live.com>
2018-06-02 13:07:32 +0600

    update

--- sim756.Net.JsonHttp/Example/Program.cs
+++ sim756.Net.JsonHttp/Example/Program.cs
@@ -15,10 +15,15 @@ namespace Example
             Example example;
             string json;
 
-            //1
+            //1 Examples - JsonHttpClient<T>
+            //1.1
+            example = new JsonHttpClient<Example>("http://www.example.com").Deserialize();
+
+            //2 Examples - JsonHttpClient
+            //2.1            
             example = JsonHttpClient.Deserialize<Example>("http://www.example.com");
 
-            //2
+            //2.2
             example = JsonHttpClient.Deserialize<Example>("http://www.example.com", new System.Net.WebClient()
             {
                 BaseAddress = "http://www.example.com",
@@ -26,13 +31,13 @@ namespace Example
                 Credentials = new Credential()
             });
 
-            //3
+            //2.3
             example = JsonHttpClient.DeserializeString<Example>("....JSON....");
 
-            //4
+            //2.4
             json = JsonHttpClient.Get("http://www.example.com");
 
-            //5
+            //2.5
             json = JsonHttpClient.Get("http://www.example.com", new System.Net.WebClient()
             {
                 BaseAddress = "http://www.example.com",
@@ -40,7 +45,7 @@ namespace Example
                 Credentials = new Credential()
             });
 
-            //6
+            //2.6
             json = JsonHttpClient.Serialize(new Example()
             {
                 Uid = 756,
